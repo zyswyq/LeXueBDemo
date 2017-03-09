66896e1f25130d951896afde13862453b34c3e44
dllo <164304651@qq.com>
2017-03-09 20:30:35 +0800

    no message

---
+++ LeXueBDemo/app/src/main/java/com/example/dllo/lexuebdemo/adapter/HomeTabAdapter.java
@@ -0,0 +1,37 @@
+package com.example.dllo.lexuebdemo.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.List;
+
+/**
+ * ✎﹏﹏﹏.₯㎕*﹏﹏﹏
+ *
+ *
+ * 　　　　 ﹏﹏﹏♥♥刘延涛✍♥♥﹏﹏
+ */
+public class HomeTabAdapter extends FragmentPagerAdapter{
+    private List <Fragment> mFragments;
+
+    public HomeTabAdapter(FragmentManager fm, List<Fragment> fragments) {
+        super(fm);
+        mFragments = fragments;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragments.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return super.getPageTitle(position);
+    }
+}
