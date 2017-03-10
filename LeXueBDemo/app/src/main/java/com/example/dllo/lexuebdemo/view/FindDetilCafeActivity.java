d0a767efc7b02c4aef02fc253c587fc9c567c440
zyswyq <763958570@qq.com>
2017-03-10 08:34:13 +0800

    no message

---
+++ LeXueBDemo/app/src/main/java/com/example/dllo/lexuebdemo/view/FindDetilCafeActivity.java
@@ -0,0 +1,31 @@
+package com.example.dllo.lexuebdemo.view;
+
+import com.example.dllo.lexuebdemo.base.BaseActivity;
+
+/**
+ * Created by dllo on 17/3/10.
+ */
+
+public class FindDetilCafeActivity extends BaseActivity{
+
+    @Override
+    protected int getLayout() {
+        return 0;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+
+}
