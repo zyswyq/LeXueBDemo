3ff607e90d87063b9ce7623fee30ce45eab8be4e
dllo <dllo@dllodeMac-mini-179.local>
2017-03-13 08:59:38 +0800

    no message

---
+++ LeXueBDemo/app/src/main/java/com/example/dllo/lexuebdemo/myself/LogonActivity.java
@@ -0,0 +1,45 @@
+package com.example.dllo.lexuebdemo.myself;
+
+import com.example.dllo.lexuebdemo.R;
+import com.example.dllo.lexuebdemo.base.BaseActivity;
+
+/*
+         |              |
+         | \            | \
+         |   | | | | | |    | | | | |||||\
+         |                          |||||||\
+         |         ( )              ||||||||
+         |                           |||||/
+         |                  | | | | | |||/
+         |    |             |          |
+         |    |             |          |
+       / |   | |            |          |\
+      |      |/             |          \|
+       \ |                  |
+         |                  |
+           \ | | | | | | | /
+             |       |            <-----弱鸡
+             |       |
+             |       |
+*/
+public class LogonActivity extends BaseActivity {
+    @Override
+    protected int getLayout() {
+        return R.layout.activity_logon;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+}
