143fc16a87370171286ca89e374d9f74457ce441
dllo <dllo@dllodeMac-mini-179.local>
2017-03-16 10:55:14 +0800

    no message

--- LeXueBDemo/app/src/main/java/com/example/dllo/lexuebdemo/myself/activity/LogonActivity.java
+++ LeXueBDemo/app/src/main/java/com/example/dllo/lexuebdemo/myself/activity/LogonActivity.java
@@ -1,7 +1,10 @@
 package com.example.dllo.lexuebdemo.myself.activity;
 
 import android.content.Intent;
+import android.text.InputType;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.example.dllo.lexuebdemo.R;
@@ -29,6 +32,9 @@ import com.example.dllo.lexuebdemo.base.BaseActivity;
 public class LogonActivity extends BaseActivity implements View.OnClickListener {
     private TextView cancelTv, clauseTv, loginTv;
     private Intent intent;
+    private ImageButton showIb;
+    private boolean isChanged = true;
+    private EditText pwdEt;
 
 
     @Override
@@ -41,7 +47,8 @@ public class LogonActivity extends BaseActivity implements View.OnClickListener 
         clauseTv = bindView(R.id.tv_fg_myself_clause);
         cancelTv = bindView(R.id.tv_logon_cancel);
         loginTv = bindView(R.id.tv_logon_login);
-
+        showIb = bindView(R.id.ib_logon_show);
+        pwdEt = bindView(R.id.et_logon_pwd);
     }
 
     @Override
@@ -54,6 +61,7 @@ public class LogonActivity extends BaseActivity implements View.OnClickListener 
         clauseTv.setOnClickListener(this);
         cancelTv.setOnClickListener(this);
         loginTv.setOnClickListener(this);
+        showIb.setOnClickListener(this);
     }
 
     @Override
@@ -71,6 +79,17 @@ public class LogonActivity extends BaseActivity implements View.OnClickListener 
                 startActivity(intent);
                 finish();
                 break;
+            case R.id.ib_logon_show:
+                if (isChanged) {
+                    showIb.setImageDrawable(getResources().getDrawable(R.mipmap.register_password_visible_icon));
+                    pwdEt.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                } else {
+                    showIb.setImageDrawable(getResources().getDrawable(R.mipmap.register_password_invisible_icon));
+                    pwdEt.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                }
+                isChanged = !isChanged;
+                break;
         }
     }
+
 }
