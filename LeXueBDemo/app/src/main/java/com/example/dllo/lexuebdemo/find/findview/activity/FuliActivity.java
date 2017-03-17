5410b7268135268fc807a73b07eb3b1b4118bbdf
zyswyq <763958570@qq.com>
2017-03-17 17:08:36 +0800

    王宇琦

--- LeXueBDemo/app/src/main/java/com/example/dllo/lexuebdemo/find/findview/activity/FuliActivity.java
+++ LeXueBDemo/app/src/main/java/com/example/dllo/lexuebdemo/find/findview/activity/FuliActivity.java
@@ -2,6 +2,7 @@ package com.example.dllo.lexuebdemo.find.findview.activity;
 
 import android.app.DownloadManager;
 import android.net.Uri;
+import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -25,6 +26,10 @@ public class FuliActivity extends BaseActivity implements View.OnClickListener {
     private String url=" http://gank.io/api/random/data/福利/1";
     private FuliBean bean;
     private String downUrl="";
+    long reference = 0L;
+    private DownloadManager downloadManager;
+
+
     @Override
     protected int getLayout() {
         return R.layout.activity_fuli;
@@ -36,6 +41,8 @@ public class FuliActivity extends BaseActivity implements View.OnClickListener {
         downbtn=bindView(R.id.btn_fuli_down);
         nextBtn=bindView(R.id.btn_fuli_next);
         bean=new FuliBean();
+        downloadManager = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+
     }
 
     @Override
@@ -56,7 +63,7 @@ public class FuliActivity extends BaseActivity implements View.OnClickListener {
                 getDaaUrl();
                 break;
             case R.id.btn_fuli_down:
-                downLoad();
+                beginDownLoad();
                 break;
         }
     }
@@ -67,18 +74,38 @@ public class FuliActivity extends BaseActivity implements View.OnClickListener {
         final DownloadManager dManager = (DownloadManager) getSystemService(this.DOWNLOAD_SERVICE);
          Uri uri = Uri.parse(downUrl);
          DownloadManager.Request request = new DownloadManager.Request(uri);
-         // 设置下载路径和文件名
-         request.setDestinationInExternalPublicDir("福利", bean.getResults().get(0).get_id()+".jpg");
+        request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_MOBILE | DownloadManager.Request.NETWORK_WIFI);
+
+        // 设置下载路径和文件名
+//         request.setDestinationInExternalPublicDir(Environment.`, bean.getResults().get(0).get_id()+".jpg");
 //         request.setDescription("软件新版本下载");
          request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
          request.setMimeType("application/vnd.android.package-archive");
          // 设置为可被媒体扫描器找到
          request.allowScanningByMediaScanner();
+        request.setShowRunningNotification(true);
+        request.setTitle("apk_title");
+        request.setDescription("apk_description");
          // 设置为可见和可管理
          request.setVisibleInDownloadsUi(true);
         dManager.equals(request);
 
     }
+    private String getFileName(String url) {
+        return url.substring(url.lastIndexOf("/"), url.length());
+    }
+    protected void beginDownLoad() {
+        DownloadManager.Request down = new DownloadManager.Request(Uri.parse(downUrl));
+        down.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_MOBILE | DownloadManager.Request.NETWORK_WIFI);
+        down.setNotificationVisibility(View.VISIBLE);
+        down.setVisibleInDownloadsUi(true);
+        down.setDestinationInExternalPublicDir(Environment.DIRECTORY_PICTURES, getFileName(downUrl));
+        down.setTitle("apk_title");
+        down.setDescription("apk_description");
+        reference = downloadManager.enqueue(down);
+        //queryDownloadStatus();
+    }
+
 
 
     private void getDaaUrl(){
